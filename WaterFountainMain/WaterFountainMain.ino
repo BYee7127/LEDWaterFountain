e52377e08aacc240b280129a4e002e91896e5404
agray0659 <agray0659@gmail.com>
2023-11-09 19:00:51 -0700

    changed naming of array

--- WaterFountainMain/WaterFountainMain.ino
+++ WaterFountainMain/WaterFountainMain.ino
@@ -32,7 +32,7 @@ AudioConnection patchCord3(mixer1, amp1);
 AudioConnection patchCord4(amp1, queue1);
 AudioConnection patchCord5(amp1, fft256);
 
-static int array[12];
+static int FFTarray[12];
 
 void setup() {
   setupLED();
@@ -70,27 +70,27 @@ void loop() {
   // analogWrite(11, 255);  // pump 01
 }
 
-void averageFFT() //averages the 128 bins of fft down to the 12 and places the values in array[]
+void averageFFT() //averages the 128 bins of fft down to 12 and places the values in FFTarray[]
 {
   for(int i=0; i<12; i++)
   {
     if(i==0) //first bin averages over 14
     {
-      array[i]=(fft256.output[0]+fft256.output[1]+fft256.output[2]+fft256.output[3]+
+      FFTarray[i]=(fft256.output[0]+fft256.output[1]+fft256.output[2]+fft256.output[3]+
                fft256.output[4]+fft256.output[5]+fft256.output[6]+fft256.output[7]+
                fft256.output[8]+fft256.output[9]+fft256.output[10]+fft256.output[11]+
                fft256.output[12]+fft256.output[13])/14;
     }
     else if(i==11) //last bin averages over 14
     {
-      array[i]=(fft256.output[114]+fft256.output[115]+fft256.output[116]+fft256.output[117]+
+      FFTarray[i]=(fft256.output[114]+fft256.output[115]+fft256.output[116]+fft256.output[117]+
                 fft256.output[118]+fft256.output[119]+fft256.output[120]+fft256.output[121]+
                 fft256.output[122]+fft256.output[123]+fft256.output[124]+fft256.output[125]+
                 fft256.output[126]+fft256.output[127])/14;
     }
     else //all other bins average over 10
     {
-      array[i]=(fft256.output[1+((i-1)*10)+13]+fft256.output[2+((i-1)*10)+13]+
+      FFTarray[i]=(fft256.output[1+((i-1)*10)+13]+fft256.output[2+((i-1)*10)+13]+
                 fft256.output[3+((i-1)*10)+13]+fft256.output[4+((i-1)*10)+13]+
                 fft256.output[5+((i-1)*10)+13]+fft256.output[6+((i-1)*10)+13]+
                 fft256.output[7+((i-1)*10)+13]+fft256.output[8+((i-1)*10)+13]+
